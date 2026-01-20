30a0a76a2362a07760ab192275145b7c6f6c5233
rahul700raj <rm2778643@gmail.com>
2026-01-20 18:25:26 +0530

    Add Tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'flipkart-blue': '#2874f0',
+        'flipkart-yellow': '#ff9f00',
+      }
+    },
+  },
+  plugins: [],
+}
